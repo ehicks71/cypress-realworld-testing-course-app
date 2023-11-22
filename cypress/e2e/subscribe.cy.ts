e226725d611cf1842e806bc4e0c9091a348fcb9d
Elizabeth <hicksem@wfu.edu>
2023-11-22 17:33:57 -0500

    Refactoring courses user journey to use data

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -5,30 +5,30 @@ describe('Newsletter Subscribe Form', () => {
   });
 
   it('allows users to subscribe to the email list', () => {
-      cy.getByData('email-input').type('test@test.com');
-      cy.getByData('submit-button').click();
-      cy.getByData('success-message')
-          .should('exist')
-          .contains('test@test.com');
+    cy.getByData('email-input').type('test@test.com');
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message')
+      .should('exist')
+      .contains('test@test.com');
   });
 
-  it.only('does NOT allow an invalid email address', () => {
-      cy.getByData('email-input').type('test');
-      cy.getByData('submit-button').click();
-      cy.getByData('success-message')
-          .should('not.exist');
+  it('does NOT allow an invalid email address', () => {
+    cy.getByData('email-input').type('test');
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message')
+      .should('not.exist');
   });
 
-    it('does NOT allow subscribing if already subscribed', () => {
-        cy.getByData('email-input').type('john@example.com');
-        cy.getByData('submit-button').click();
-        cy.getByData('success-message')
-            .should('not.exist');
-        cy.getByData('server-error-message')
-            .should('exist')
-            .contains('john@example.com');
-        cy.getByData('server-error-message')
-            .should('exist')
-            .contains('already exists. Please use a different email address.');
-    });
+  it('does NOT allow subscribing if already subscribed', () => {
+    cy.getByData('email-input').type('john@example.com');
+    cy.getByData('submit-button').click();
+    cy.getByData('success-message')
+      .should('not.exist');
+    cy.getByData('server-error-message')
+      .should('exist')
+      .contains('john@example.com');
+    cy.getByData('server-error-message')
+      .should('exist')
+      .contains('already exists. Please use a different email address.');
+  });
 });
