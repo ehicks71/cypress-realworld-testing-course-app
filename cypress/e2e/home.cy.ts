6b5ff867e986eafa81aa6ad2bd7e8175fae55c08
Elizabeth <hicksem@wfu.edu>
2023-11-22 15:19:29 -0500

    Refactoring tests to use data json file

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,34 @@
+// @ts-ignore
+import aCourses from '../fixtures/courses.json';
+
+describe('Home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  context('Hero section', () => {
+    it('the h1 contains the correct text', () => {
+      cy.getByData('hero-heading')
+        .contains('Testing Next.js Applications with Cypress');
+    });
+
+    it('the features on the homepage are correct', () => {
+      cy.get('dt').eq(0).contains('4 Courses');
+      cy.get('dt').eq(1).contains('25+ Lessons');
+      cy.get('dt').eq(2).contains('Free and Open Source');
+    });
+  });
+
+  context('Course section', () => {
+    aCourses.forEach((oCourse: {title: string, slug: string, lessons: {}[] }, iCourseIndex: number) => {
+      it(`Course: "${oCourse.title}"`, () => {
+        cy.getByData(`course-${iCourseIndex}`)
+            .find('a')
+            .contains('Get started')
+            .click();
+        cy.location('pathname')
+            .should('equal', `/${oCourse.slug}`);
+      });
+    });
+  });
+});
---
+++ cypress/e2e/user-journey.cy.ts
@@ -0,0 +1,52 @@
+// @ts-ignore
+
+import aCourses from '../fixtures/courses.json';
+
+describe('User Journey', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  context('Completing courses found on the homepage', () => {
+    // Iterate over each course.
+    aCourses.forEach((oCourse: {title: string, slug: string, lessons: {}[] }, iCourseIndex: number) => {
+      it(`user can find course "${oCourse.title}" on the home page and complete the course lessons`, () => {
+
+        // Click on the course link based on the lesson index
+        cy.getByData(`course-${iCourseIndex}`)
+          .find('a')
+          .contains('Get started')
+          .click();
+        cy.location('pathname').should('equal', `/${oCourse.slug}`);
+        cy.getByData('next-lesson-button')
+          .contains('Start Course')
+          .click();
+        const aCourseLessons= oCourse.lessons;
+
+        // Iterate over each lesson in the course.
+        aCourseLessons.forEach((oLesson: {slug: string}, iLessonIndex: number) => {
+          const sLessonPath = `/${oCourse.slug}/${oLesson.slug}`;
+          cy.location('pathname').should('eq', sLessonPath);
+          cy.getByData('challenge-answer-0').click();
+
+          // If it's not the last lesson, click on "Next Lesson" button
+          if (iLessonIndex < aCourseLessons.length - 1) {
+            cy.getByData('next-lesson-button')
+              .should('exist')
+              .contains('Next Lesson')
+              .click();
+          }
+        });
+
+        // When all lessons finished, click on "Complete Course" button
+        cy.getByData('next-lesson-button')
+          .should('exist')
+          .contains('Complete Course')
+          .click();
+        cy.location('pathname').should('eq', '/');
+      });
+    });
+
+  });
+
+});
